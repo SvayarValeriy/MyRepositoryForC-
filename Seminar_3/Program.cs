c2b0569676b88e2cfe761933e42753eab54c8a72
valeriy <svayar@ya.ru>
2023-01-12 19:41:24 +0300

    Закончено домашнее задание.

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -1,15 +1,55 @@
 ﻿/*
-void GetDiapazone(int quadrant)
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+
+
+
+int x1 = ReadInt("Введите х для точки А1: ");
+int y1 = ReadInt("Введите y для точки А2: ");
+int z1 = ReadInt("Введите z для точки А3: ");
+int x2 = ReadInt("Введите х для точки В1: ");
+int y2 = ReadInt("Введите y для точки В2: ");
+int z2 = ReadInt("Введите z для точки В3: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z2 - z1;
+
+double distance = Math.Sqrt(A * A + B * B + C * C);
+distance = Math.Round(length, 2);
+Console.WriteLine("Расстояние = " + distance);
+
+
+int ReadInt(string message)
 {
-    if (quadrant == 1) Console.WriteLine("x > 0 and y > 0");
-    else if (quadrant == 2) Console.WriteLine("x < 0 and y > 0");
-    else if (quadrant == 3) Console.WriteLine("x < 0 and y < 0");
-    else if (quadrant == 4) Console.WriteLine("x > 0 and y < 0");
-    else Console.WriteLine("Wrong input");
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
-Console.WriteLine("Input a number of quadrant:");
-int quadNum = Convert.ToInt32(Console.ReadLine());
+*/
 
-GetDiapazone(quadNum);
+
+/*
+//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов
+// чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <=n; i++)
+{
+  Console.Write($"{i * i * i}, ");
+}
+
+
+//Второй вариант, с применением math, только не получилось сделать вывод в 1 строку.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <=n; i++)
+{
+  Console.WriteLine(Math.Pow(i, 3));
+}
+
 */
